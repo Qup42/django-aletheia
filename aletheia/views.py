Qup42/django-aletheia

commit 3135610ec5e25b61c88f692e5eb84f74f6239d52

Implement Remember Me and Next functionality

aletheia/views.py
@@ -1,21 +1,17 @@
-import json
 import logging
 import secrets
-from json import JSONDecodeError
 
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse
 from django.http.response import HttpResponseRedirectBase
-from django.views import View
 from django.views.generic.base import TemplateResponseMixin
-from django.views.generic.edit import FormMixin
+from django.views.generic.edit import FormMixin, ProcessFormView
 from webauthn import (
     generate_registration_options,
     verify_registration_response,
     options_to_json,
-    base64url_to_bytes, generate_authentication_options
+    generate_authentication_options
 )
 from webauthn.helpers.exceptions import InvalidRegistrationResponse
 from webauthn.helpers.structs import (
@@ -30,7 +26,7 @@ from webauthn.helpers.structs import (
 from aletheia import settings
 from aletheia.forms import WebAuthNLoginForm
 from aletheia.models import AuthData
-from aletheia.util import base64encode, base64decode
+from aletheia.util import base64encode, base64decode, get_request_param, get_next_redirect_url
 
 
 class HttpUnprocessableEntity(HttpResponseRedirectBase):
@@ -122,34 +118,40 @@ def login_config(request):
     return HttpResponse(content=options_to_json(options), content_type="application/json")
 
 
-class LoginView(View, FormMixin, TemplateResponseMixin):
+class LoginView(TemplateResponseMixin, FormMixin, ProcessFormView):
     template_name = "webauthn/login.html"
     form_class = WebAuthNLoginForm
+    success_url = None
+    redirect_field_name = "next"
 
-    def get(self, request, *args, **kwargs):
-        """Handle GET requests: instantiate a blank version of the form."""
-        return self.render_to_response(self.get_context_data())
+    def get_form_kwargs(self):
+        kwargs = super(LoginView, self).get_form_kwargs()
+        kwargs["request"] = self.request
+        return kwargs
 
-    def post(self, request, *args, **kwargs):
-        try:
-            login_response_json = json.loads(request.body.decode("utf-8"))
-        except JSONDecodeError as error:
-            message = f"Login failed. Failed parsing JSON. Error: {error}"
-            messages.error(request, message, fail_silently=True)
-            logger.warning(f"Failed parsing JSON. Error: {error}")
-            return HttpUnprocessableEntity(message)
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        return form.login(self.request, redirect_url=success_url)
 
-        user = authenticate(request,
-                            # TODO: this dict access might fail
-                            credential_id=base64encode(base64url_to_bytes(login_response_json["id"])),
-                            data=request.body.decode("utf-8"))
-        if user is None:
-            return HttpResponseForbidden(f"Login failed.")
+    def get_success_url(self):
+        # Explicitly passed ?next= URL takes precedence
+        ret = (
+                get_next_redirect_url(self.request, self.redirect_field_name)
+                or self.success_url
+        )
+        return ret
 
-        login(request, user)
-        messages.success(request, f"Successfully signed in as {user.username}.", fail_silently=True)
+    def get_context_data(self, **kwargs):
+        ret = super(LoginView, self).get_context_data(**kwargs)
+        redirect_field_value = get_request_param(self.request, self.redirect_field_name)
 
-        return HttpResponse("Success")
+        ret.update(
+            {
+                "redirect_field_name": self.redirect_field_name,
+                "redirect_field_value": redirect_field_value,
+            }
+        )
+        return ret
 
 
 login_view = LoginView.as_view()

aletheia/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
+from webauthn import base64url_to_bytes
+
+from aletheia import settings
+from aletheia.util import base64encode
 
 
 class WebAuthNLoginForm(forms.Form):
@@ -9,3 +16,43 @@ class WebAuthNLoginForm(forms.Form):
     client_id = forms.CharField(widget=forms.HiddenInput())
     webauthn_data = forms.CharField(widget=forms.HiddenInput())
     remember = forms.BooleanField(label=_("Remember Me"), required=False)
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request", None)
+        super(WebAuthNLoginForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        super(WebAuthNLoginForm, self).clean()
+        if self._errors:
+            return
+
+        credential_id = self.cleaned_data["client_id"]
+        webauthn_data = self.cleaned_data["webauthn_data"]
+
+        user = authenticate(self.request,
+                            credential_id=base64encode(base64url_to_bytes(credential_id)),
+                            data=webauthn_data)
+
+        if user:
+            self.user = user
+        else:
+            raise forms.ValidationError("Login Failed")
+
+        return self.cleaned_data
+
+    def login(self, request, redirect_url=None):
+        login(request, self.user)
+        messages.success(request, f"Successfully signed in as {self.user.username}.", fail_silently=True)
+
+        remember = settings.SESSION_REMEMBER
+        if remember is None:
+            remember = self.cleaned_data["remember"]
+        if remember:
+            # None means global expiry setting -> SESSION_COOKIE_AGE
+            request.session.set_expiry(None)
+        else:
+            request.session.set_expiry(0)
+
+        response = HttpResponseRedirect(redirect_url)
+
+        return response

aletheia/settings.py
@@ -16,3 +16,7 @@ EXPECTED_ORIGIN = getattr(
 FORCE_PASSWORD_TIMEOUT = getattr(
     settings, "WEBAUTHN_FORCE_PASSWORD_TIMEOUT", relativedelta(months=6)
 )
+
+SESSION_REMEMBER = getattr(
+    settings, "WEBAUTHN_SESSION_REMEMBER", None
+)

aletheia/templates/webauthn/login.html
@@ -35,7 +35,7 @@
             <a href="{{ signup_url }}">sign up</a> first.{% endblocktrans %}</p>
     {% endif %}
 
-    <form class="login" onsubmit="event.preventDefault(); login_webauth(username.value)">
+    <form id="form_login" class="login" onsubmit="event.preventDefault(); login_webauth(username.value)" method="POST" action="{% url "aletheia:login" %}">
         {% csrf_token %}
         {{ form.as_p }}
         {% if redirect_field_value %}

aletheia/templates/webauthn/scripts.html
@@ -111,21 +111,9 @@
             }
         }
 
-        await fetch("{% url "aletheia:login" %}",
-            {
-                "method": "POST",
-                "headers": {"X-CSRFToken": Cookies.get('csrftoken')},
-                "mode": "same-origin",
-                "body": JSON.stringify(encoded)
-            })
-            .then(response => {
-                if (response.status === 200) {
-                    window.location.replace("{% url "index" %}");
-                    login_status_callback("success", undefined);
-                } else {
-                    login_status_callback("fail", response.statusText);
-                }
-            })
+        document.getElementById("{{ form.client_id.id_for_label }}").value = credential.id;
+        document.getElementById("{{ form.webauthn_data.id_for_label }}").value = JSON.stringify(encoded);
+        document.getElementById("form_login").submit();
     }
 
 </script>

aletheia/util.py
@@ -1,4 +1,5 @@
 import base64
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 def base64encode(s: bytes) -> str:
@@ -8,3 +9,21 @@ def base64encode(s: bytes) -> str:
 def base64decode(s: str) -> bytes:
     return base64.b64decode(s)
 
+
+def get_request_param(request, param, default=None):
+    """
+    Gets the parameter param from request. The parameter may be passed as GET or POST parameter.
+    """
+    if request is None:
+        return default
+    return request.POST.get(param) or request.GET.get(param, default)
+
+
+def get_redirect_url(request, redirect_field_name="next"):
+    """
+    Gets the redirect url for a request. It is passed as GET or POST parameter "next".
+    """
+    redirect_to = get_request_param(request, redirect_field_name)
+    if not url_has_allowed_host_and_scheme(redirect_to, allowed_hosts=None, require_https=True):
+        redirect_to = None
+    return redirect_to

commit 0a5654db1f81620803b4bf0b0eb1869e92deb17e

Fix naming error introduced by 3135610e

aletheia/views.py
@@ -26,7 +26,7 @@ from webauthn.helpers.structs import (
 from aletheia import settings
 from aletheia.forms import WebAuthNLoginForm
 from aletheia.models import AuthData
-from aletheia.util import base64encode, base64decode, get_request_param, get_next_redirect_url
+from aletheia.util import base64encode, base64decode, get_request_param, get_redirect_url
 
 
 class HttpUnprocessableEntity(HttpResponseRedirectBase):
@@ -136,7 +136,7 @@ class LoginView(TemplateResponseMixin, FormMixin, ProcessFormView):
     def get_success_url(self):
         # Explicitly passed ?next= URL takes precedence
         ret = (
-                get_next_redirect_url(self.request, self.redirect_field_name)
+                get_redirect_url(self.request, self.redirect_field_name)
                 or self.success_url
         )
         return ret